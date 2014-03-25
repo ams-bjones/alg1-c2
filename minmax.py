07bea6f577281710dad95db4eb8601e57207dcec
Brian Jones <bjones@ashmanorschool.com>
2014-03-25 11:09:47 -0400

    minmax completed

--- minmax.py
+++ minmax.py
@@ -6,3 +6,23 @@ def mini(inList):
         if item < answer:
             answer = item
     return (answer)
+
+def maxi(inList):
+    answer = None
+    for item in inList:
+        if answer == None:
+            answer = item
+        if item > answer:
+            answer = item
+    return (answer)
+
+def inList(size):
+    inlist = []
+    for i in range (size):
+        inlist.append( int(input('please enter number {0} >'.format(i+1))))
+    return (inlist)
+    
+myList = inList(5)
+print (myList)
+print (maxi(myList))
+print(mini(myList))
--- minmaxtest.py
+++ minmaxtest.py
@@ -12,6 +12,15 @@ class TestSequenceFunctions(unittest.TestCase):
         inpu = [2,3,4,5,6]
         expMin = 2
         assert minmax.mini(inpu) == expMin #Test 2 Fails
-
+    
+    def test3(self):
+        inpu = [5,4,3,2,1]
+        expMin = 1
+        assert minmax.mini(inpu) == expMin #Test 3 Fails
+    
+    def test4(self):
+        inpu = [1,2,3,4,5]
+        expMax = 5
+        assert minmax.maxi(inpu) == expMax #Test 4 Fails
 if __name__ == '__main__':
     unittest.main()
